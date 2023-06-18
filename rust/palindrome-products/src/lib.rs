a82823505bd9ca1b3fe87e0f4eb81758ec26ea85
Darrell Roberts <droberts0674@gmail.com>
2023-06-18 12:56:37 -0400

    update

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -12,9 +12,6 @@ pub struct Palindrome(u64);
 impl Palindrome {
     /// Create a `Palindrome` only if `value` is in fact a palindrome when represented in base ten. Otherwise, `None`.
     pub fn new(value: u64) -> Option<Palindrome> {
-        // Using this iterator is a bit slower vs
-        // the in place algorithm commented out below.
-        // ~ 7 seconds.
         let digit_iter = DigitIterator::new(value);
         let half = digit_iter.len() / 2;
 
@@ -22,25 +19,6 @@ impl Palindrome {
             .take(half)
             .eq(digit_iter.rev().take(half))
             .then_some(Self(value))
-
-        // A faster algorithm. ~ 3 seconds.
-
-        // let mut result = value;
-        // let mut divisor = 1;
-        // while (value / divisor) >= 10 {
-        //     divisor *= 10;
-        // }
-
-        // while result != 0 {
-        //     let leading = result / divisor;
-        //     let trailing = result % 10;
-        //     if leading != trailing {
-        //         return None;
-        //     }
-        //     result = (result % divisor) / 10;
-        //     divisor /= 100;
-        // }
-        // Some(Self(value))
     }
 
     /// Get the value of this palindrome.
@@ -50,16 +28,28 @@ impl Palindrome {
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    let (highest, lowest) = (min..=max)
-        .filter(|n| n % 10 != 0)
-        .flat_map(|x| (x..=max).filter(|n| n % 10 != 0).map(move |y| (x, y)))
-        .map(|(x, y)| x * y)
-        .flat_map(Palindrome::new)
-        .fold((None, None), |(h, l), n| match (h, l) {
-            (Some(h), Some(_)) if n > h => (Some(n), l),
-            (Some(_), Some(l)) if n < l => (h, Some(n)),
-            (None, None) => (Some(n), Some(n)),
-            _ => (h, l),
+    let mut prod_min = u64::MAX;
+    let mut prod_max = u64::MIN;
+
+    let (lowest, highest) = (min..=max)
+        .flat_map(|x| (x..=max).map(move |y| x * y))
+        .fold((None, None), |(l, h), prod| {
+            if prod > prod_min && prod < prod_max {
+                return (l, h);
+            }
+
+            match Palindrome::new(prod) {
+                Some(p) if prod_min > prod => {
+                    prod_min = prod;
+                    (Some(p), h)
+                }
+                Some(p) if prod_max < prod => {
+                    prod_max = prod;
+                    (l, Some(p))
+                }
+                _ => (l, h),
+            }
         });
+
     lowest.and_then(|l| highest.map(|h| (l, h)))
 }
